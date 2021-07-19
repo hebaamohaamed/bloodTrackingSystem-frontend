bd12dd63a61bfe4c4799245035b340332931d191
ayaelbadrawey <ubuntu@ubun2004.linuxvmimages.local>
2021-07-19 18:01:21 -0400

    axios added in hospital-bag-recieved and hospital-bag-used

--- src/components/contents/hospital/Hospital-Bag-Recieved.js
+++ src/components/contents/hospital/Hospital-Bag-Recieved.js
@@ -1,48 +1,80 @@
-import { findDOMNode } from 'react-dom';
-import {useHistory} from 'react-router-dom'
 import HospitalHeader from '../../headers/hospital'
 import $ from 'jquery'
 import { Component } from 'react';
+import axios from 'axios';
 
+export default class HospitalBagRecieved extends Component{
+    constructor(props){
+      super(props)
+      this.state={
+        bNumber: null,
+      }
+    }
 
-class hospitalBagRecieved extends Component{
-  handleButton = ()=>{
-    const button = findDOMNode(this.refs.confirm);
-    $(button).css("color", "#C31313");
-    $(button).css("background-color", "#FFFAFA");
-    $(button).css("font-weight", "bold");
-    $(button).css("border-radius", "5px");
-    $(button).html("Confirmed");
-    $(button).attr("class","fas fa-check");
-    $(button).css("text-transform","capitalize");
-  }
- render(){
-return(
-    <div>
-    <HospitalHeader/>
-    <div id="rec">
-    <div class="login-box">
-    <h2>Info Needed</h2>
-    <form>
-      <div className="user-box">
-        <input type="text" name="" required=""/>
-        <label>Enter Blood Bag ID</label>
-      </div>
-      <a id="confirm" onClick={this.handleButton} ref="confirm" >
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Confirm
-      </a>
-    </form>
-  </div>
-  </div>
-  </div>
+    TrigerAxios(event){
+      event.preventDefault();
+      const currentDate2 = new Date();
+      const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+      const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+      const currentDate = date + " " + time  
+      axios.get(`http://localhost:5001/second/state?id=${this.state.bNumber}&time=${currentDate}`)
+      .then(response =>{
+        let output = Object.values(response.data);
+        alert("Blood Bag State: Delievered")
+        this.TrigerAxios2(event);
+        console.log("Change State Confirmed")
+      })
+      .catch(error=>{
+        console.log("TEST ERROR", error)        
+      })
 
+    }
+    TrigerAxios2(event){
+      event.preventDefault();
+      const currentDate2 = new Date();
+      const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+      const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+      const currentDate = date + " " + time  
+      axios.get(`http://localhost:5001/change/location?id=${this.state.bNumber}&loc=HOSPITAL&oid=BB102&time=${currentDate}`)
+      .then(response =>{
+        let output = Object.values(response.data);
+        let objectOutput = JSON.parse(output[0]);
+        console.log("RESPONSE", objectOutput.DIN);
+        alert("Blood Bag Location: Hospital")
+      })
+      .catch(error=>{
+        console.log("TEST ERROR", error)
+      })
+    }
+    handleInoutChange(value){
+      this.setState({
+        bNumber: value
+      })
+    }
 
-);
+    render(){
+    return(
+      <div>
+      <HospitalHeader/>
+      <div id="rec">
+      <div class="login-box">
+      <h2>Info Needed</h2>
+      <form >
+        <div className="user-box">
+          <input type="text" value={this.state.bNumber} onChange={(e) =>{this.handleInoutChange(e.target.value)}}/>
+          <label>Enter Blood Bag ID</label>
+        </div>
+        <a id="confirm" onClick={(event)=>this.TrigerAxios(event)}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Confirm
+        </a>        
+      </form>
+    </div>
+    </div>
+    </div>  
+    );
+      }
 }
-}
-
-export default hospitalBagRecieved;
--- src/components/contents/hospital/Hospital-Bag-Used.js
+++ src/components/contents/hospital/Hospital-Bag-Used.js
@@ -3,7 +3,86 @@ import {useHistory} from 'react-router-dom'
 import { Component } from "react";
 import $ from 'jquery'
 import { findDOMNode } from 'react-dom';
+import axios from 'axios'
 class hospitalBagUsed extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      bNumber: null,
+      patientID: null,
+      processID: null
+    }
+  }
+
+  TrigerAxios(event){
+    event.preventDefault();
+    const currentDate2 = new Date();
+    const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+    const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+    const currentDate = date + " " + time  
+    axios.get(`http://localhost:5001/third/state?id=${this.state.bNumber}&pid=${this.state.patientID}&time=${currentDate}`)
+    .then(response =>{
+      let output = Object.values(response.data);
+      alert("Blood Bag State: Used")
+      this.TrigerAxios2(event);
+      console.log("Change State Confirmed")
+    })
+    .catch(error=>{
+      console.log("TEST ERROR", error)
+      
+    })
+
+  }
+  TrigerAxios2(event){
+    event.preventDefault();
+    const currentDate2 = new Date();
+    const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+    const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+    const currentDate = date + " " + time  
+    axios.get(`http://localhost:5001/change/location?id=${this.state.bNumber}&loc=PATIENT&oid=${this.state.patientID}&time=${currentDate}`)
+    .then(response =>{
+      let output = Object.values(response.data);
+      let objectOutput = JSON.parse(output[0]);
+      this.TrigerAxios3(event);
+      console.log("RESPONSE", objectOutput.DIN);
+      alert("Blood Bag Location: Patient")
+    })
+    .catch(error=>{
+      console.log("TEST ERROR", error)
+    })
+  }
+  TrigerAxios3(event){
+    event.preventDefault();
+    const currentDate2 = new Date();
+    const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+    const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+    const currentDate = date + " " + time  
+    axios.get(`http://localhost:5001/create/process?pin=${this.state.processID}&id=${this.state.bNumber}&uid=${this.state.patientID}&oid=H102&type=recieve&time=${currentDate}`)
+    .then(response =>{
+      let output = Object.values(response.data);
+      let objectOutput = JSON.parse(output[0]);
+      console.log("RESPONSE", objectOutput.DIN);
+      alert("Recieve Process: Created")
+    })
+    .catch(error=>{
+      console.log("TEST ERROR", error)
+    })
+  }
+  handleInoutChange(value){
+    this.setState({
+      bNumber: value
+    })
+  }
+  handleInoutChange2(value2){
+    this.setState({
+      patientID: value2
+    })
+  }
+  handleInoutChange3(value3){
+    this.setState({
+      processID: value3
+    })
+  }
     usedButton2 = () =>{
       const b = findDOMNode(this.refs.submit2);
         $(b).css("color", "#C31313");
@@ -30,45 +109,30 @@ return(
     <div>
         <HospitalHeader />
          <div className="new-box">
-    <h2>New Patient</h2>
+    <h2>Info Needed</h2>
     <form>
+    <div className="new-user-box">
+        <input type="text" value={this.state.processID} onChange={(e) =>{this.handleInoutChange3(e.target.value)}}/>
+        <label>Enter Process ID</label>
+      </div>
       <div className="new-user-box">
-        <input type="text" name="" required=""/>
+        <input type="text" value={this.state.patientID} onChange={(e) =>{this.handleInoutChange2(e.target.value)}}/>
         <label>Enter Patient E-Mail</label>
       </div>
       <div className="new-user-box">
-        <input type="text" name="" required=""/>
+        <input type="text" value={this.state.bNumber} onChange={(e) =>{this.handleInoutChange(e.target.value)}}/>
         <label>Enter Blood Bag ID</label>
       </div>
       <div className="new-user-box">
         <input type="number" name="" required=""/>
         <label>Enter number of Blood Bags</label>
       </div>
-      <a id="button1" href="#" onClick={this.usedButton2} ref="submit2" >
+      <a id="button1" href="#"onClick={(event)=>this.TrigerAxios(event)} >
         Submit
       </a>
     </form>
   </div>
-  <div class="existing-box">
-    <h2>Existing Patient</h2>
-    <form>
-      <div className="existing-user-box">
-        <input type="text" name="" required=""/>
-        <label>Enter Patient E-Mail</label>
-      </div>
-      <div className="existing-user-box">
-        <input type="text" name="" required=""/>
-        <label>Enter Blood Bag ID</label>
-      </div>
-      <div className="existing-user-box">
-        <input type="number" name="" required=""/>
-        <label>Enter number of Blood Bags</label>
-      </div>
-      <a id="button2" href="#" onClick={this.usedButton} ref="submit">
-        Submit
-      </a>
-    </form>
-  </div>
+
   </div>
    
 );
