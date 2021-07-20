0ed5ebde46a0335f8c8ef0c62caa39b936a531b7
ayaelbadrawey <ubuntu@ubun2004.linuxvmimages.local>
2021-07-20 13:24:31 -0400

    Adding Axios to Blood-Bank-SendBag page

--- src/components/contents/bloodbank/Blood-Bank-SendBag.js
+++ src/components/contents/bloodbank/Blood-Bank-SendBag.js
@@ -2,8 +2,58 @@ import { Component } from "react";
 import BloodBankHeader from "../../headers/bloodbank";
 import $ from 'jquery'
 import { findDOMNode } from 'react-dom';
+import axios from 'axios'
 
 class bloodBankSendBags extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      bNumber: null,
+    }
+  }
+
+  TrigerAxios(event){
+    event.preventDefault();
+    const currentDate2 = new Date();
+    const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+    const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+    const currentDate = date + " " + time  
+    axios.get(`http://localhost:5000/first/state?id=${this.state.bNumber}&time=${currentDate}`)
+    .then(response =>{
+      let output = Object.values(response.data);
+      alert("Blood Bag State: Under Transportation")
+      this.TrigerAxios2(event);
+      console.log("Change State Confirmed")
+    })
+    .catch(error=>{
+      console.log("TEST ERROR", error)
+      
+    })
+  }
+  TrigerAxios2(event){
+    event.preventDefault();
+    const currentDate2 = new Date();
+    const date = currentDate2.getDate() +'/'+(currentDate2.getMonth()+1) +'/'+currentDate2.getFullYear()
+    const time = currentDate2.getHours() +':'+currentDate2.getMinutes() +':'+currentDate2.getSeconds()
+    const currentDate = date + " " + time  
+    let ownerID = "BB101";
+    axios.get(`http://localhost:5000/change/location?id=${this.state.bNumber}&oid=${ownerID}&time=${currentDate}`)
+    .then(response =>{
+      let output = Object.values(response.data);
+      let objectOutput = JSON.parse(output[0]);
+      alert("Blood Bag Location: Transportation Car")
+    })
+    .catch(error=>{
+      console.log("TEST ERROR", error)
+    })
+  }
+  handleInputChange(value){
+    this.setState({
+      bNumber: value
+    })
+  }
+
+
     button =()=>{
         const button1 = findDOMNode(this.refs.submit);
         $(button1).css("color", "#C31313");
@@ -22,10 +72,10 @@ class bloodBankSendBags extends Component{
     <h2>Info Needed</h2>
     <form>
       <div className="user-box">
-        <input type="text" name="" required=""/>
+        <input type="text" required="" value={this.state.bNumber} onChange={(e) =>{this.handleInputChange(e.target.value)}}/>
         <label>Enter Blood Bag ID</label>
       </div>
-      <a id="button" href="#" onClick={this.button} ref="submit">
+      <a id="button" href="#" onClick={(event)=>this.TrigerAxios(event)} ref="submit">
         <span></span>
         <span></span>
         <span></span>
